e03939e7a2078a555c703495eacdfa702248530e
Agamenton <ofgagamenton@seznam.cz>
2025-06-18 12:02:57 +0200

    git init main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+
+WORKSHOP_ID = "233860" # Steam Workshop ID for Kenshi
+
+
+def main():
+    ...
+    # discover local mods
+    # discover workshop mods
+    # instantiate Mod objects
+    # start GUI
+    
+
+if __name__ == "__main__":
+    main()
