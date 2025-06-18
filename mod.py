26d502497fc8693d8f97cec5fb5fa65dbe6330f2
Agamenton <ofgagamenton@seznam.cz>
2025-06-18 12:16:08 +0200

    git init mod.py

---
+++ mod.py
@@ -0,0 +1,9 @@
+
+class Mod:
+    def __init__(self, path):
+        self.path = None
+        self.name = None
+        self.version = None
+        self.author = None
+        self.description = None
+        self.mod_id = None
