ae24c6e2ab83232d448cfafa21150f4c1ae5dff6
Agamenton <ofgagamenton@seznam.cz>
2025-07-04 19:47:55 +0200

    updated build process to not use --onefile and start virtual env first

--- build.bat
+++ build.bat
@@ -1,5 +1,15 @@
 @ECHO OFF
 
+
+set "venvPath=%~dp0.venv\Scripts\activate.bat"
+if not exist "%venvPath%" (
+    echo Virtual environment not found. Please create it first.
+    echo Run: python -m venv .venv
+    pause
+    exit /b 1
+)
+call "%venvPath%"
+
 set "pathToMain=%~dp0"
 set "nameOfMain=main"
 set "exeName=KenPy"
@@ -12,7 +22,6 @@ if exist "%pathToMain%%nameOfMain%.exe" (
 )
 
 pyinstaller.exe ^
-    --onefile ^
     --noconsole ^
     --noupx ^
     --clean ^
@@ -34,9 +43,7 @@ if exist %pathToMain%__pycache__\ (
 if exist %pathToMain%build\ (
     rmdir /S /Q "%pathToMain%build"
 )
-if exist %pathToMain%dist\ (
-    rmdir /S /Q "%pathToMain%dist"
-)
+
 if exist %pathToMain%%nameOfMain%.spec (
     del "%pathToMain%%nameOfMain%.spec"
 )
